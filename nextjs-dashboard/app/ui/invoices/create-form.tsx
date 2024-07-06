dd6eb98240d3957043b53b49dd61e6f0e8d8156f
takeyuki1253 <takeyuki.u.1253@gmail.com>
2024-07-06 15:55:09 +0900

    Chapter.17 end
    a
    aa

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -15,10 +15,10 @@ import { useActionState } from 'react';
 export default function EditInvoiceForm({
   invoice,
   customers,
-}: {
+  }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
+  }) {
   const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
